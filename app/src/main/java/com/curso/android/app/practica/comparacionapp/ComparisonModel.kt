adb2192b52789b31af6a0616d974871d8e4ea88d
PabloPeralta94 <pablo.peralta94@gmail.com>
2023-08-13 22:44:25 -0300

    prueba de clases kotlin

---
+++ app/src/main/java/com/curso/android/app/practica/comparacionapp/ComparisonModel.kt
@@ -0,0 +1,7 @@
+package com.curso.android.app.practica.comparacionapp.model
+
+class ComparisonModel {
+    fun compareStrings(text1: String, text2: String): Boolean {
+        return text1 == text2
+    }
+}
--- app/src/main/java/com/curso/android/app/practica/comparacionapp/MainActivity.kt
+++ app/src/main/java/com/curso/android/app/practica/comparacionapp/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.curso.android.app.practica.comparacionapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.curso.android.app.practica.comparacionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: CompareViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,35 +19,28 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        viewModel = ViewModelProvider(this).get(CompareViewModel::class.java)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.compareButton.setOnClickListener {
+            val text1 = binding.editText1.text.toString()
+            val text2 = binding.editText2.text.toString()
+            viewModel.compareStrings(text1, text2)
         }
+
+        viewModel.result.observe(this, Observer { result ->
+            binding.resultTextView.text = result
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
